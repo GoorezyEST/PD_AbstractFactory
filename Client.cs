4eda60d09adac64b0bb25408f8370209034284f0
GoorezyEST <goorezyestudios@gmail.com>
2023-02-11 17:49:16 -0300

    Added comments

--- Client.cs
+++ Client.cs
@@ -7,17 +7,20 @@ using System.Threading.Tasks;
 
 namespace AbstractFactory_PD
 {
+    //Concrete class that uses the abstract factory pattern to create objects
     class Client
     {
         private readonly Shirt _shirt;
         private readonly Trousers _trousers;
 
+        //Constructor that takes in a ClothesFactory and uses it to create instances of Shirt and Trousers
         public Client(ClothesFactory factory)
         {
             _shirt = factory.CreateShirt();
             _trousers = factory.CreateTrousers();
         }
 
+        //Method that returns a string describing the client's clothes
         public string DecribeYourClothes()
         {
             return $"Today I'm dressed in: {_shirt.GetType().Name} and {_trousers.GetType().Name}";
